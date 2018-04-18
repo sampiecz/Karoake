4bbbf27dae6dc698e4caa6fbf0707afcb7e8d8d0
sampiecz <samuel.piecz@gmail.com>
2018-04-18 16:18:55 -0500

    Create tables. Kinda.

---
+++ karoake.sql
@@ -0,0 +1,20 @@
+###########################################################
+# CSCI 466 - Group Project - Spring 2018                  #
+#                                                         #
+# Progammer: Sam Piecz                                    #
+# Z-ID: Z1732715                                          #
+# Section: 3                                              #
+# TA: Rajarshi Sen                                        #
+# Date Due: Apr 30, 2018                                  # 
+# Purpose: Karoake web app for djs.                       # 
+###########################################################
+
+CREATE TABLE Customer( customerId integer AUTO_INCREMENT, name Char(20), primary key(ownerId);
+CREATE TABLE Singer( singerId integer AUTO_INCREMENT, singer Char(20), drummer Char(20), guitarist Char(20));
+CREATE TABLE Artist( artistId integer AUTO_INCREMENT, );
+CREATE TABLE Songs( songId integer AUTO_INCREMENT, );
+CREATE TABLE Dj( djId integer AUTO_INCREMENT, );
+CREATE TABLE FreeQueue();
+CREATE TABLE PriorityQueue();
+CREATE TABLE Transaction();
+
