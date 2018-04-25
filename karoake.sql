e8b17f294599becdf893576cdacfd3c051a57ef9
sampiecz <samuel.piecz@gmail.com>
2018-04-25 15:36:08 -0500

    Updated togroups sql.

--- karoake.sql
+++ karoake.sql
@@ -8,13 +8,9 @@
 # Date Due: Apr 30, 2018                                  # 
 # Purpose: Karoake web app for djs.                       # 
 ###########################################################
-
-CREATE TABLE Customer( customerId integer AUTO_INCREMENT, name Char(20), primary key(ownerId);
-CREATE TABLE Singer( singerId integer AUTO_INCREMENT, singer Char(20), drummer Char(20), guitarist Char(20));
-CREATE TABLE Artist( artistId integer AUTO_INCREMENT, );
-CREATE TABLE Songs( songId integer AUTO_INCREMENT, );
-CREATE TABLE Dj( djId integer AUTO_INCREMENT, );
-CREATE TABLE FreeQueue();
-CREATE TABLE PriorityQueue();
-CREATE TABLE Transaction();
-
+CREATE TABLE Singer( singerId integer AUTO_INCREMENT, name Char(20), primary key(ownerId), paid bool() );
+CREATE TABLE Band( bandId integer AUTO_INCREMENT,  bandmemberId int, FOREIGN KEY (songId) REFERENCES Song (songId) );
+CREATE TABLE Song( songId integer AUTO_INCREMENT, title char(20), contributorId int, FOREIGN KEY (contributorId) REFERENCES Contributor (contributorId), );
+CREATE TABLE Contributor( contributorId integer AUTO_INCREMENT, songId int, singerId int, FOREIGN KEY (songId) REFERENCES Song (songId), FOREIGN KEY (singerId) REFERENCES (singerId));
+CREATE TABLE KaroakeFile( fileId integer AUTO_INCREMENT, singerId int, songId int, contributorId int, FOREIGN KEY (singerId) REFERENCES Singer (singerId), FOREIGN KEY (songId) REFERENCES Song (songId));
+CREATE TABLE Queue( queueId integer AUTO_INCREMENT, position int, paid bool, fileId int, FOREIGN KEY (fileId) REFERENCES Karoake (fileId), );
