6b458cf3370f1e6e16d7865a1fb443b264ebfeb9
sampiecz <samuel.piecz@gmail.com>
2018-04-25 17:10:59 -0500

    Fixed request table.

--- karoake.sql
+++ karoake.sql
@@ -9,8 +9,8 @@
 # Purpose: Karoake web app for djs.                       # 
 ###########################################################
 
-CREATE TABLE Band ( bandId integer AUTO_INCREMENT, name Char(255));
-CREATE TABLE Song ( songId integer AUTO_INCREMENT, name Char(255), bandId int, FOREIGN KEY (bandId) REFERENCES Band (bandId));
-CREATE TABLE BandMember( bandMemberId integer AUTO_INCREMENT, name Char(255), instrument Char(255), bandId int, FOREIGN KEY (bandId) REFERENCES Band (bandId));
-CREATE TABLE Requester ( requesterId integer AUTO_INCREMENT, name Char(255)); 
-CREATE TABLE Request ( requestId integer AUTO_INCREMEMNT, paid bool, amountpaid int, songId int, requesterId int, FOREIGN KEY (songId) REFERENCES Song (songId), FOREIGN KEY (requesterId) REFERENCES Requester (reqeusterId)); 
+CREATE TABLE Band ( bandId integer AUTO_INCREMENT, name Char(255), primary key (bandId));
+CREATE TABLE Song ( songId integer AUTO_INCREMENT, name Char(255), bandId int, primary key (songId), FOREIGN KEY (bandId) REFERENCES Band (bandId));
+CREATE TABLE BandMember( bandMemberId integer AUTO_INCREMENT, name Char(255), instrument Char(255), bandId int, primary key (bandMemberId), FOREIGN KEY (bandId) REFERENCES Band (bandId));
+CREATE TABLE Requester ( requesterId integer AUTO_INCREMENT, name Char(255), primary key (requesterId)); 
+CREATE TABLE Request ( requestId integer AUTO_INCREMENT, paid BOOL, amountpaid int, songId int, requesterId int, hasplayed BOOL, primary key (requestId), FOREIGN KEY (songId) REFERENCES Song (songId), FOREIGN KEY (requesterId) REFERENCES Requester (requesterId)); 
