b1a372a825f0a950ee4bca105d52be687f86063e
sampiecz <samuel.piecz@gmail.com>
2018-04-25 16:42:20 -0500

    Added amount paid attribute to request table.

--- karoake.sql
+++ karoake.sql
@@ -13,4 +13,4 @@ CREATE TABLE Band ( bandId integer AUTO_INCREMENT, name Char(255));
 CREATE TABLE Song ( songId integer AUTO_INCREMENT, name Char(255), bandId int, FOREIGN KEY (bandId) REFERENCES Band (bandId));
 CREATE TABLE BandMember( bandMemberId integer AUTO_INCREMENT, name Char(255), instrument Char(255), bandId int, FOREIGN KEY (bandId) REFERENCES Band (bandId));
 CREATE TABLE Requester ( requesterId integer AUTO_INCREMENT, name Char(255)); 
-CREATE TABLE Request ( requestId integer AUTO_INCREMEMNT, paid bool, songId int, requesterId int, FOREIGN KEY (songId) REFERENCES Song (songId), FOREIGN KEY (requesterId) REFERENCES Requester (reqeusterId)); 
+CREATE TABLE Request ( requestId integer AUTO_INCREMEMNT, paid bool, amountpaid int, songId int, requesterId int, FOREIGN KEY (songId) REFERENCES Song (songId), FOREIGN KEY (requesterId) REFERENCES Requester (reqeusterId)); 
