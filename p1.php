79747c88825182e1bbdfe50c2a30a32ebc891d0a
sampiecz <samuel.piecz@gmail.com>
2018-04-30 14:58:13 -0500

    Fixed p1 & p2 kinda

--- p1.php
+++ p1.php
@@ -3,12 +3,11 @@
 <h1> Song Search Page</h1>
 <h3> Please search for desired song using any of the search options below</h3>
 
-<form method="POST" action="p2.php">
+<form method="POST" action="/~z1732715/group_project/p2.php">
 <p> Enter Song title: <input type = 'text' name ='title'/> </p>
 <p> Enter Artist Name: <input type='text' name='artist'/> </p>
 <p> Contributor Name: <input type='text' name='contr'/></p>
 <p> <input type='Submit'/>
-
 
 </form>
 </body>
--- p2.php
+++ p2.php
@@ -3,20 +3,22 @@
 <body>
 <h1 align='center'> Karaoke Pg 2 </h1>
 <p align='center'> Please choose song version and add it to desired queue, you may choose to pay and get your song played early </p>
+
+<!-- Connect to the database -->
 <?php
-
-try
-{
-	$dsn="mysql:host=courses; dbname=z1732715";
-	$pdo = new PDO($dsn, 'z1732715', '1996Apr23');
-}
-
-catch(PDOexception $e)
-{
-	echo 'Connection to databse failed' .$e->getmessage();
-}	
-
+    try {
+        $dsn = "mysql:host=courses;dbname=z1732715";
+        $username = "z1732715";
+        $password = "1996Apr23";
+        $pdo = new PDO($dsn, $username, $password);
+    }
+    catch(PDOexception $e) {
+        echo "Connection to database failed: " . $e->getMessage();
+    }
 ?>
+<!-- End Connect to the database -->
+
+
 <table border="1" width ="50">
 
 
@@ -58,12 +60,89 @@ foreach($rows as $res)
 ?>
 
 </table>
+<!-- Start test -->
 
 <?php
 
-// ATTACH ADAM"S CODE HERE 
+  //Placeholder
+  //requester should equal requesterId from search
+  $requester = '1';
+
+
+  //Choose Song Form
+  echo"<form method='post'>";
+
+  //Placeholder for songs gotten from search
+  //Loop through songs displayed in radio buttons
+  //name=song value=songId
+  echo"<table>
+      <tr>
+        <td>
+      <label>Song Name</label><input type='radio' name='song' value='1'></td></tr>";
+
+  //Choose paid or unpaid
+  echo"
+    <tr><td>Queue</td></tr>
+    <tr><td><input type='radio' name='queue' value='0'> Free Queue<td><tr>
+    <tr><td><input type='radio' name='queue' value='1'> Priority Queue<td>
+    <td><input type='text' name='amountPaid'/></td></tr>
+    <tr><td><input type='Submit' name='Submit' value='Submit'/></td></tr>
+    <tr><td><input type='reset' value='Reset'/></td></tr>
+</form>";
+
+//When submit button is hit
+if(isset($_POST['Submit']))
+{
+
+  //Check for choice of song
+  if(isset($_POST['song']) )
+  {
+
+  //Check for choie of queue
+  if(isset($_POST['queue']) )
+  {
+
+  //get queue selection
+  $queueChoice = $_POST['queue'];
+
+  $paidChoice = '0';
+
+  //set amount paid to written amount if input
+  if($_POST['amountPaid'] > '0'){
+$paidChoice = $_POST['amountPaid'];
+}
+
+  //if no amount input, default to free queue
+  else{
+$queueChoice = '0';
+  }
+
+  //set song choice to chosen song
+  $songChoice = $_POST['song'];
+
+  //Zero for hasplayed input
+  $zero = '0';
+
+//sql statement to insert into queue
+$sql = "INSERT INTO Request (paid, amountpaid, songId, requesterId, hasplayed) VALUES (:paid, :amountpaid, :songId, :requesterId, :hasplayed)";
+
+//Prepare the variables to be inserted into the sql statement
+$prepared = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+
+
+//Execute the sql statement with the variables
+$result = $prepared->execute(array(':paid' => $queueChoice, ':amountpaid' => $paidChoice, ':songId' => $songChoice, ':requesterId' => $requester, ':hasplayed' => $zero));
+
+}//Queue Choice if
+else{ echo"<p align='center' style='color:red;'>*No Queue selected</p>";}
+
+}//Song Choice if
+else{ echo"<p align='center' style='color:red;'>*No song selected</p>";}
+
+}//submit button if
 
 ?>
+<!-- End test -->
 
 </body>
 </html>
